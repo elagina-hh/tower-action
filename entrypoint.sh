61c2c3768e8981f08d8709d3fa7db37196d53c74
Liudmila <lelagina@harbinger-health.com>
2022-08-03 20:18:01 +0000

    setting flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,10 +23,11 @@ fi
 tw \
     launch \
     $PIPELINE \
+    ${INSECURE_FLAG} \
     --params-file=params.json \
     ${WORKDIR:+"--work-dir=$WORKDIR"} \
     ${TOWER_COMPUTE_ENV:+"--compute-env=$TOWER_COMPUTE_ENV"} \
     ${REVISION:+"--revision=$REVISION"} \
     ${CONFIG_PROFILES:+"--profile=$CONFIG_PROFILES"} \
     ${PRE_RUN_SCRIPT:+"--pre-run=pre_run.sh"} \
-    ${NEXTFLOW_CONFIG:+"--config=nextflow.config"} ${INSECURE_FLAG}
+    ${NEXTFLOW_CONFIG:+"--config=nextflow.config"}
