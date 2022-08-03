4099ef2bdb7aa5a7db93f15b6f89d8c107ce2a0f
Liudmila <lelagina@harbinger-health.com>
2022-08-03 20:01:31 +0000

    Updating to most recent version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,13 @@
 tw info
 
 # Print the params input to a file
-echo $PARAMETERS > params.json
+echo -e "$PARAMETERS" > params.json
 
-# Hack to convert profiles JSON to csv
-CONFIG_PROFILES_CSV=$(echo $CONFIG_PROFILES | jq -r '. | @csv' | sed 's/"//g')
+# Print the pre-run script to a file
+echo -e "$PRE_RUN_SCRIPT" > pre_run.sh
+
+# Print the nextflow config  to a file
+echo -e "$NEXTFLOW_CONFIG" > nextflow.config
 
 # Check if need to allow to connect to a non-SSL secured Tower server
 INSECURE_FLAG=""
@@ -17,10 +20,13 @@ if [[ "$INSECURE" == "true" ]]; then
 fi
 
 # Launch the pipeline
-tw launch $PIPELINE \
-    --params=params.json \
+tw \
+    launch \
+    $PIPELINE \
+    --params-file=params.json \
     ${WORKDIR:+"--work-dir=$WORKDIR"} \
     ${TOWER_COMPUTE_ENV:+"--compute-env=$TOWER_COMPUTE_ENV"} \
     ${REVISION:+"--revision=$REVISION"} \
-    ${CONFIG_PROFILES_CSV:+"--profile=$CONFIG_PROFILES_CSV"} ${INSECURE_FLAG}
-    
+    ${CONFIG_PROFILES:+"--profile=$CONFIG_PROFILES"} \
+    ${PRE_RUN_SCRIPT:+"--pre-run=pre_run.sh"} \
+    ${NEXTFLOW_CONFIG:+"--config=nextflow.config"} ${INSECURE_FLAG}
